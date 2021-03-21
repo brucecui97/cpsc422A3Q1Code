269c9853253a12ef8d130d771e80ff6a045cf3c7
bruce <brucec@hotmail.com>
2021-03-21 16:31:18 -0700

    finailize code to generate plot as well

--- Gibbs_Samping.m
+++ Gibbs_Samping.m
@@ -2,7 +2,7 @@
 %where index 0 being true is a = t, index 1 being true is b = t, etc.
 
 samples = [0,1,0,0]
-for iteration = [1:10^5]
+for iteration = [1:5*10^5]
     iteration
     last_sample = samples(end,:);
     new_sample = last_sample;
@@ -33,7 +33,21 @@ end
 
 %%
 
-num_As = sum(samples(:,1));
-num_samples = length(samples)
+prob_estimate = zeros(1,length(samples));
+for num_samples_used = [1:length(samples)]
+    num_samples_used
+    sub_samples = samples(1:num_samples_used,:);
+    num_As = sum(sub_samples(:,1));
+    
+    prob_estimate(num_samples_used) = num_As/num_samples_used;
+end
 
-probAGivenb1 = num_As/num_samples
+%%
+plot(prob_estimate(1:num_samples_used-1))
+hold on
+yline(0.05659);
+
+xlabel("number of samples");
+ylabel("Prob estimate from Gibbs Sampling");
+title("Prob estimate from Gibbs Sampling vs number of samples used ")
+legend("gibbs", "VE");
