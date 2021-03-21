cfde89bfee508fb7ae9fddd43bd37242ac6b2493
bruce <brucec@hotmail.com>
2021-03-21 15:57:28 -0700

    fix bug of not starting with b = 1

--- Gibbs_Samping.m
+++ Gibbs_Samping.m
@@ -1,7 +1,7 @@
 %let a sample be represented as list[bool] of size 4
 %where index 0 being true is a = t, index 1 being true is b = t, etc.
 
-samples = [0,0,0,0]
+samples = [0,1,0,0]
 for iteration = [1:10^5]
     iteration
     last_sample = samples(end,:);
@@ -11,18 +11,17 @@ for iteration = [1:10^5]
     else
         new_sample(1)=0;
     end
-    samples(end+1,:) = new_sample;
     
-    last_sample = samples(end,:);
+    last_sample = new_sample;
     new_sample = last_sample;
     if rand<getProbCGivenDB(last_sample(4),last_sample(2))
         new_sample(3) = 1;
     else
         new_sample(3)=0;
     end
-    samples(end+1,:) = new_sample;
+
     
-    last_sample = samples(end,:);
+    last_sample = new_sample;
     new_sample = last_sample;
     if rand<getProbDGivenAC(last_sample(1),last_sample(3))
         new_sample(4) = 1;
